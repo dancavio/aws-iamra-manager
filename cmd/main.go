edb562514721fec7bcf7609f8c3c28e213f30068
Dan Cavallaro <dan.t.cavallaro@gmail.com>
2025-04-17 13:45:05 -0400

    WIP stupid OIDC test script

--- cmd/main.go
+++ cmd/main.go
@@ -1,187 +1,274 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2015-2021 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
 import (
-	"crypto/tls"
-	"dancav.io/aws-iamra-manager/internal/build"
+	"bytes"
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	"os"
+	"github.com/minio/minio-go/v7"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"golang.org/x/oauth2"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
-	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
-
-	"dancav.io/aws-iamra-manager/api/v1"
-	"dancav.io/aws-iamra-manager/internal/controller"
-	webhookv1 "dancav.io/aws-iamra-manager/internal/webhook/v1"
-	// +kubebuilder:scaffold:imports
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
+
+// Returns a base64 encoded random 32 byte string.
+func randomState() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.RawURLEncoding.EncodeToString(b)
+}
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	stsEndpoint    string
+	configEndpoint string
+	clientID       string
+	clientSec      string
+	clientScopes   string
+	port           int
 )
 
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+// DiscoveryDoc - parses the output from openid-configuration
+// for example http://localhost:8080/auth/realms/minio/.well-known/openid-configuration
+type DiscoveryDoc struct {
+	Issuer                           string   `json:"issuer,omitempty"`
+	AuthEndpoint                     string   `json:"authorization_endpoint,omitempty"`
+	TokenEndpoint                    string   `json:"token_endpoint,omitempty"`
+	UserInfoEndpoint                 string   `json:"userinfo_endpoint,omitempty"`
+	RevocationEndpoint               string   `json:"revocation_endpoint,omitempty"`
+	JwksURI                          string   `json:"jwks_uri,omitempty"`
+	ResponseTypesSupported           []string `json:"response_types_supported,omitempty"`
+	SubjectTypesSupported            []string `json:"subject_types_supported,omitempty"`
+	IDTokenSigningAlgValuesSupported []string `json:"id_token_signing_alg_values_supported,omitempty"`
+	ScopesSupported                  []string `json:"scopes_supported,omitempty"`
+	TokenEndpointAuthMethods         []string `json:"token_endpoint_auth_methods_supported,omitempty"`
+	ClaimsSupported                  []string `json:"claims_supported,omitempty"`
+	CodeChallengeMethodsSupported    []string `json:"code_challenge_methods_supported,omitempty"`
+}
 
-	utilruntime.Must(v1.AddToScheme(scheme))
-	// +kubebuilder:scaffold:scheme
+func parseDiscoveryDoc(ustr string) (DiscoveryDoc, error) {
+	d := DiscoveryDoc{}
+	req, err := http.NewRequest(http.MethodGet, ustr, nil)
+	if err != nil {
+		return d, err
+	}
+	clnt := http.Client{
+		Transport: http.DefaultTransport,
+	}
+	resp, err := clnt.Do(req)
+	if err != nil {
+		return d, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return d, fmt.Errorf("unexpected error returned by %s : status(%s)", ustr, resp.Status)
+	}
+	dec := json.NewDecoder(resp.Body)
+	if err = dec.Decode(&d); err != nil {
+		return d, err
+	}
+	return d, nil
+}
+
+func init() {
+	flag.StringVar(&stsEndpoint, "sts-ep", "http://localhost:9000", "STS endpoint")
+	flag.StringVar(&configEndpoint, "config-ep",
+		"http://localhost:8080/auth/realms/minio/.well-known/openid-configuration",
+		"OpenID discovery document endpoint")
+	flag.StringVar(&clientID, "cid", "", "Client ID")
+	flag.StringVar(&clientSec, "csec", "", "Client Secret")
+	flag.StringVar(&clientScopes, "cscopes", "openid", "Client Scopes")
+	flag.IntVar(&port, "port", 8080, "Port")
+}
+
+func implicitFlowURL(c oauth2.Config, state string) string {
+	var buf bytes.Buffer
+	buf.WriteString(c.Endpoint.AuthURL)
+	v := url.Values{
+		"response_type": {"id_token"},
+		"response_mode": {"form_post"},
+		"client_id":     {c.ClientID},
+	}
+	if c.RedirectURL != "" {
+		v.Set("redirect_uri", c.RedirectURL)
+	}
+	if len(c.Scopes) > 0 {
+		v.Set("scope", strings.Join(c.Scopes, " "))
+	}
+	v.Set("state", state)
+	v.Set("nonce", state)
+	if strings.Contains(c.Endpoint.AuthURL, "?") {
+		buf.WriteByte('&')
+	} else {
+		buf.WriteByte('?')
+	}
+	buf.WriteString(v.Encode())
+	return buf.String()
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	var secureMetrics bool
-	var enableHTTP2 bool
-	var tlsOpts []func(*tls.Config)
-	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
-		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	flag.BoolVar(&secureMetrics, "metrics-secure", true,
-		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
-	flag.BoolVar(&enableHTTP2, "enable-http2", false,
-		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
-	setupLog.Info(fmt.Sprintf("AWS IAM RA Manager version %s", build.ReleaseVersion))
-
-	// if the enable-http2 flag is false (the default), http/2 should be disabled
-	// due to its vulnerabilities. More specifically, disabling http/2 will
-	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
-	// Rapid Reset CVEs. For more information see:
-	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
-	// - https://github.com/advisories/GHSA-4374-p667-p6c8
-	disableHTTP2 := func(c *tls.Config) {
-		setupLog.Info("disabling http/2")
-		c.NextProtos = []string{"http/1.1"}
+	if clientID == "" {
+		flag.PrintDefaults()
+		return
 	}
 
-	if !enableHTTP2 {
-		tlsOpts = append(tlsOpts, disableHTTP2)
-	}
-
-	webhookServer := webhook.NewServer(webhook.Options{
-		TLSOpts: tlsOpts,
-	})
-
-	// Metrics endpoint is enabled in 'config/default/kustomization.yaml'. The Metrics options configure the server.
-	// More info:
-	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/metrics/server
-	// - https://book.kubebuilder.io/reference/metrics.html
-	metricsServerOptions := metricsserver.Options{
-		BindAddress:   metricsAddr,
-		SecureServing: secureMetrics,
-		TLSOpts:       tlsOpts,
-	}
-
-	if secureMetrics {
-		// FilterProvider is used to protect the metrics endpoint with authn/authz.
-		// These configurations ensure that only authorized users and service accounts
-		// can access the metrics endpoint. The RBAC are configured in 'config/rbac/kustomization.yaml'. More info:
-		// https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/metrics/filters#WithAuthenticationAndAuthorization
-		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
-
-		// If CertDir, CertName, and KeyName are not specified, controller-runtime will automatically
-		// generate self-signed certificates for the metrics server. While convenient for development and testing,
-		// this setup is not recommended for production.
-	}
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		Metrics:                metricsServerOptions,
-		WebhookServer:          webhookServer,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "6b2e7301.dancav.io",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
-	})
+	ddoc, err := parseDiscoveryDoc(configEndpoint)
+	log.Printf("%v\n", ddoc)
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
+		log.Println(fmt.Errorf("Failed to parse OIDC discovery document %s", err))
+		fmt.Println(err)
+		return
 	}
 
-	if err = (&controller.AwsIamRaRoleProfileReconciler{
-		Client:     mgr.GetClient(),
-		Scheme:     mgr.GetScheme(),
-		Recorder:   mgr.GetEventRecorderFor("iamram-controller"),
-		KubeConfig: mgr.GetConfig(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "AwsIamRaRoleProfile")
-		os.Exit(1)
+	scopes := ddoc.ScopesSupported
+	if clientScopes != "" {
+		scopes = strings.Split(clientScopes, ",")
 	}
-	// nolint:goconst
-	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-		if err = webhookv1.SetupAwsIamRaRoleProfileWebhookWithManager(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "AwsIamRaRoleProfile")
-			os.Exit(1)
+
+	ctx := context.Background()
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSec,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  ddoc.AuthEndpoint,
+			TokenURL: ddoc.TokenEndpoint,
+		},
+		RedirectURL: fmt.Sprintf("http://127.0.0.1:%d/oauth2/callback", port),
+		Scopes:      scopes,
+	}
+
+	state := randomState()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s\n", r.Method, r.RequestURI)
+		if r.RequestURI != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		if clientSec != "" {
+			http.Redirect(w, r, config.AuthCodeURL(state), http.StatusFound)
+		} else {
+			http.Redirect(w, r, implicitFlowURL(config, state), http.StatusFound)
+		}
+	})
+
+	http.HandleFunc("/oauth2/callback", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s\n", r.Method, r.RequestURI)
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if r.Form.Get("state") != state {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
 		}
 
-		if err = webhookv1.SetupPodWebhookWithManager(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "Pod")
-			os.Exit(1)
+		var getWebTokenExpiry func() (*credentials.WebIdentityToken, error)
+		if clientSec == "" {
+			getWebTokenExpiry = func() (*credentials.WebIdentityToken, error) {
+				return &credentials.WebIdentityToken{
+					Token: r.Form.Get("id_token"),
+				}, nil
+			}
+		} else {
+			getWebTokenExpiry = func() (*credentials.WebIdentityToken, error) {
+				oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+				if err != nil {
+					return nil, err
+				}
+				if !oauth2Token.Valid() {
+					return nil, errors.New("invalid token")
+				}
+
+				log.Printf("%v\n", oauth2Token)
+
+				return &credentials.WebIdentityToken{
+					Token:  oauth2Token.Extra("id_token").(string),
+					Expiry: int(oauth2Token.Expiry.Sub(time.Now().UTC()).Seconds()),
+				}, nil
+			}
 		}
-	}
-	// +kubebuilder:scaffold:builder
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
-		os.Exit(1)
-	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
-		os.Exit(1)
-	}
+		sts, err := credentials.NewSTSWebIdentity(stsEndpoint, getWebTokenExpiry)
+		if err != nil {
+			log.Println(fmt.Errorf("Could not get STS credentials: %s", err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
-		os.Exit(1)
-	}
+		opts := &minio.Options{
+			Creds:        sts,
+			BucketLookup: minio.BucketLookupAuto,
+		}
+
+		u, err := url.Parse(stsEndpoint)
+		if err != nil {
+			log.Println(fmt.Errorf("Failed to parse STS Endpoint: %s", err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		clnt, err := minio.New(u.Host, opts)
+		if err != nil {
+			log.Println(fmt.Errorf("Error while initializing Minio client, %s", err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		buckets, err := clnt.ListBuckets(r.Context())
+		if err != nil {
+			log.Println(fmt.Errorf("Error while listing buckets, %s", err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		creds, _ := sts.Get()
+
+		bucketNames := []string{}
+
+		for _, bucket := range buckets {
+			log.Println(fmt.Sprintf("Bucket discovered: %s", bucket.Name))
+			bucketNames = append(bucketNames, bucket.Name)
+		}
+		response := make(map[string]interface{})
+		response["credentials"] = creds
+		response["buckets"] = bucketNames
+		c, err := json.MarshalIndent(response, "", "\t")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(c)
+	})
+
+	address := fmt.Sprintf(":%v", port)
+	log.Printf("listening on http://%s/", address)
+	log.Fatal(http.ListenAndServe(address, nil))
 }
